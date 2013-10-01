e8c5e1ba8343a52e13ea0a6f9185c3dab787f056
jiacongh <hejiacongtheone@gmail.com>
2013-10-01 15:01:04 -0400

    1

---
+++ hw2-jiacongh/src/main/java/edu/cmu/deiis/annotator/Consumer.java
@@ -0,0 +1,23 @@
+package edu.cmu.deiis.annotator;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.resource.ResourceProcessException;
+
+public class Consumer extends CasConsumer_ImplBase {
+
+  @Override
+  public void processCas(CAS aCAS) throws ResourceProcessException {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
